d9a0ff55805b843b051db86fe81747c34eb1cc08
Omprasadbl <omprasadbl@gmail.com>
2025-12-22 12:09:50 +0530

    updating

--- app/api/store/dashboard/route.js
+++ app/api/store/dashboard/route.js
@@ -1,7 +1,7 @@
 // get total order total earnings and total products
 import prisma from "@/lib/prisma";
 import authSeller from "@/middlewares/authSeller";
-import { getAuth } from "@clerk/nextjs/dist/types/server";
+import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
